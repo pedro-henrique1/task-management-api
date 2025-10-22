1e4187b3e608d5e78f71c8beb92acb681141dce9
pedro henrique silva rodrigues <ph8464803@gmail.com>
2025-10-21 22:11:55 -0300

    refactor: ajust to pipeline test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { IncomingMessage } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 import { LoggerModule } from 'nestjs-pino';
 import configuration from './modules/config/configuration';
 import { validationSchema } from './modules/config/validation';
@@ -10,6 +10,7 @@ import { TaskModule } from './modules/tasks/task.module';
 
 interface PinoRequest extends IncomingMessage {
   user?: { id: number };
+  id?: string;
 }
 
 @Module({
@@ -29,15 +30,18 @@ interface PinoRequest extends IncomingMessage {
     LoggerModule.forRoot({
       pinoHttp: {
         level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
-        transport:
-          process.env.NODE_ENV === 'development'
-            ? { target: 'pino-pretty' }
-            : undefined,
+        transport: process.env.NODE_ENV === 'development' ? { target: 'pino-pretty' } : undefined,
         serializers: {
-          req: (req) => ({ method: req.method, url: req.url }),
-          res: (res) => ({ statusCode: res.statusCode }),
+          req: (req: IncomingMessage) => ({
+            method: req.method,
+            url: req.url,
+          }),
+          res: (res: ServerResponse) => ({
+            statusCode: res.statusCode,
+          }),
         },
-        genReqId: (req) => req.headers['x-request-id'] || crypto.randomUUID(),
+        genReqId: (req: IncomingMessage) =>
+          (req.headers['x-request-id'] as string) || crypto.randomUUID(),
         customProps: (req: PinoRequest) => ({
           requestId: req.id,
           userId: req.user?.id,
