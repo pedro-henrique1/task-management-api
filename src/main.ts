05f7ca9bb1c9cbfd608d0bac57de2c5021797f5a
pedro henrique silva rodrigues <ph8464803@gmail.com>
2025-10-25 21:48:40 -0300

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Task API')
+    .setDescription('API for managing tasks')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
